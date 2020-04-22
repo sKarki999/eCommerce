c2b7b144eaeb0e08448bd92abc13ffb851f71444
Sagun <sagun.karki27@gmail.com>
2020-04-22 20:07:31 +0200

    Shopping cart status integrated.

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -27,7 +29,8 @@ export class ProductListComponent implements OnInit {
 
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(()=> {
@@ -145,6 +148,18 @@ export class ProductListComponent implements OnInit {
 
 
 
+  addToCart(theProduct: Product){
+
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    // TODO .... do the real work
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
+
+    
+
+  }
 
 
 
---
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+
+  addToCart(theCartItem: CartItem){
+
+  // check if we already have the item in the cart
+  let alreadyExistsInCart: boolean = false;
+  let existingCartItem: CartItem = undefined;  
+
+  if( this.cartItems.length>0 ){
+  
+    // find the item in the card based on item id
+    for(let tempCartItem of this.cartItems){
+      if(tempCartItem.id === theCartItem.id){
+        existingCartItem = tempCartItem;
+        break;
+      }
+    }
+
+    // check if we found it
+    alreadyExistsInCart = (existingCartItem != undefined);
+
+  }
+
+  if(alreadyExistsInCart) {
+    existingCartItem.quantity++;
+  }
+  else {
+    this.cartItems.push(theCartItem);
+  }
+
+  this.computeCartTotals();
+
+}
+  computeCartTotals() {
+
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for(let currentCartItem of this.cartItems){
+
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+
+    }
+
+    // publish new values, all subscribers will receive the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+  
+    
+    // log cart data just for debugging purposes
+    this.logCartData(totalPriceValue);
+  
+  }
+
+  logCartData(totalPriceValue: number) {
+
+    console.log('Content of the Cart');
+    for(let tempCartItem of this.cartItems){
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
+      console.log(`name:${tempCartItem.name}, quantity:${tempCartItem.quantity}, unitPrice:${tempCartItem.unitPrice}, 
+                   SubTotalPrice:${subTotalPrice}`);
+    }
+
+    console.log(`Total Price:${totalPriceValue.toFixed(2)}, Total Quantity=${this.totalQuantity}`);
+    console.log('---------');
+  }
+
+}
